34be9d9e2544628384bca911f3142e149f6cb93b
Chrystiam <chrystiamgg@misena.edu.co>
2013-05-02 08:56:47 -0500

    commit de inicio para el proyecto se adicionan las gemas a utilizar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FEDCIM::Application.config.secret_token = 'ca779f30709b0dff9201ba9cda42394b77fdd0577506bc2c896d5a14365b2ee097e387ff07f838fc53c5c6940bf459f5f55c4f2a486de15e8644966793a2a78e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FEDCIM::Application.config.session_store :cookie_store, key: '_FEDCIM_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FEDCIM::Application.config.session_store :active_record_store
